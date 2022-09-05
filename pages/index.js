433a9b366b19ade484415edd5349179d342d0da2
AkeelEdwards <35719583+AkeelEdwards@users.noreply.github.com>
2022-09-04 21:52:42 -0400

    c

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,14 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export async function getStaticProps () {
+  const req = await fetch ('/some-api');
+  const car = await req.json();
+
+
+}
+
+export default function Car() {
   return (
     <div className="container">
       <Head>
